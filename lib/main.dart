a66f06016dd261e0f991b6f0711711a93ad8d3d0
Airton Neto <netoairton0@gmail.com>
2022-08-26 17:04:57 -0300

    Add card widget

--- lib/main.dart
+++ lib/main.dart
@@ -11,18 +11,62 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.white,
+        backgroundColor: Colors.blue[200],
         appBar: AppBar(
-          title: Text('Alfa'),
+          title: Text(
+            'CENOPS',
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
           centerTitle: true,
           backgroundColor: Colors.blueAccent[400],
         ),
         body: SafeArea(
-          child: Container(
-            width: 150.0,
-            height: 150.0,
-            color: Colors.grey,
-            child: Text('Logo Cenops'),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                width: 150.0,
+                height: 150.0,
+                color: Colors.grey,
+                child: Text('Logo Cenops'),
+              ),
+              Padding(
+                padding: EdgeInsets.all(17.0),
+                child: Card(
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.medical_services,
+                      color: Colors.amber,
+                    ),
+                    title: Text(
+                      'Médicos',
+                    ),
+                    horizontalTitleGap: 10.0,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(17.0),
+                child: Card(
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.gavel,
+                      color: Colors.amber,
+                    ),
+                    title: Text(
+                      'Advogados',
+                    ),
+                    horizontalTitleGap: 10.0,
+                  ),
+                ),
+              ),
+              Container(
+                width: double.infinity,
+              ),
+            ],
           ),
         ),
       ),
